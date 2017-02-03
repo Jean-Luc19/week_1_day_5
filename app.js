6b17fcff38986a117857ac4c3fd44fa627b971c9
James Guthrie <jamesrguthrie@gmail.com>
2017-02-03 11:42:49 -0500

    first Iteration of shopping list

--- app.js
+++ app.js
@@ -1,1 +1,60 @@
-console.log("testing again, again");
+  
+
+  var state = {
+
+    tasks: [ { task: 'apple', completed: false },
+             { task: 'oranges', completed: false },
+             { task: 'milk', completed: true } 
+            ],
+
+    removeTask: function(task) {
+
+    },
+
+    renderList: function(tasks) {
+      var shoppingList = $('.shopping-list');
+      var listItem = '<li>';
+      tasks.forEach(function(current) {
+        listItem += '<span class="shopping-item">' + current.task + '</span>' + listItemEnd;
+
+      });
+      shoppingList.append(listItem);
+
+    }
+
+
+  }
+
+
+  $('#js-shopping-list-form').on('click', 'button', function(e) {
+    e.preventDefault();
+    var userInput = $('#shopping-list-entry').val();
+  });
+
+
+
+
+  var listItemEnd = '<div class="shopping-item-controls">' +
+          '<button class="shopping-item-toggle">' +
+            '<span class="button-label">check</span>' +
+          '</button>' +
+          '<button class="shopping-item-delete">' +
+            '<span class="button-label">delete</span>' +
+          '</button>' +
+        '</div>' +
+      '</li>';
+
+
+
+
+
+
+
+
+
+$(function() {
+  state.renderList(state.tasks);
+
+
+
+});
